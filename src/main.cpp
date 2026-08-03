d50f38afa618234498bdd09b07e5039f36a65a99
knightgriffon-hash <knightgriffon@gmail.com>
2026-08-03 21:51:40 +0200

    Current state: BLE beacon test with errors

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,39 @@
 #include <Arduino.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLEBeacon.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define DEVICE_NAME "AMP_ACTION_BTN"
+#define BEACON_UUID "12345678-1234-1234-1234-123456789abc"
+
+BLEAdvertising *pAdvertising;
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+    Serial.begin(115200);
+    Serial.println("BLE Action Button Test (no button)");
+
+    BLEDevice::init(DEVICE_NAME);
+    BLEServer *pServer = BLEDevice::createServer();
+    pAdvertising = pServer->getAdvertising();
+
+    BLEBeacon beacon;
+    beacon.setManufacturerId(0x004C);
+    beacon.setProximityUUID(BLEUUID(BEACON_UUID));
+    beacon.setMajor(0);
+    beacon.setMinor(1);
+
+    pAdvertising->setAdvertisementData(beacon.getData());
+    pAdvertising->start();
+
+    Serial.println("BLE Beacon advertising started.");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(100);
+    digitalWrite(LED_BUILTIN, LOW);
+    
+    Serial.println("Beacon sent!");
+    delay(5000);
 }
